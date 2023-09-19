dd7701d058b4d26082033aa44ca144882d4b5fea
yzhao3685 <yunfan@LAPTOP-PPOLIQ6M>
2023-09-18 23:55:31 -0400

    armman set up complete

--- run/job.run_rmabppo_armman.sh
+++ run/job.run_rmabppo_armman.sh
@@ -1,9 +1,9 @@
 data="armman"
 save_string="armman_rmabppo_test"
-N=5
-B=1.0
+N=25
+B=5.0
 robust_keyword="sample_random" # other option is "mid"
-n_train_epochs=10
+n_train_epochs=100
 seed=0
 cdir="."
 no_hawkins=1
